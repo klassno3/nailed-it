43fa6b3b115eae95ee25fd3566a59d1017bb49da
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-15 09:23:48 +0300

    Add every component to the fullpage

--- nailed-it/src/HomePage/FullPage/FullPage.jsx
+++ nailed-it/src/HomePage/FullPage/FullPage.jsx
@@ -1,4 +1,22 @@
+import ContactUs from "../ContactUs/ContactUs";
+import Footer from "../Footer/Footer";
+import Gallary from "../Gallary/Gallary";
+import Hero from "../Hero/Hero";
+import Nav from "../Nav/Nav";
+import Services from "../Services/Services";
+import Testimonial from "../Testimonial/Testimonial";
+
 const FullPage = () => {
-  return <div className="text-2xl">FullPage</div>;
+  return (
+    <div>
+      <Nav />
+      <Hero />
+      <Services />
+      <Gallary />
+      <Testimonial />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
 };
 export default FullPage;
